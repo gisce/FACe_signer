a8279403e81fa91ec99b3f543cc953feedac710b
Xavi Torelló <info@xaviertorello.cat>
2017-12-14 12:20:37 +0100

    Add package setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import FACe_signer
+
+INSTALL_REQUIRES = ['zeep', 'soap_wsse']
+
+setup(
+    name='FACe signer',
+    description='Zeep plugin that signs FACe SOAP requests',
+    version=FACe_signer.__version__,
+    url='https://www.gisce.net',
+    author='Xavi Torelló',
+    author_email='xtorello@gisce.net',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    license='General Public Licence 3',
+    provides=['FACe_signer'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
