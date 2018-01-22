c5d26ebc78e4435669b51076a0dc597801e400e0
Xavi Torelló <info@xaviertorello.cat>
2018-01-22 12:40:24 +0100

    Update to 0.1.1

--- FACe_signer/__init__.py
+++ FACe_signer/__init__.py
@@ -2,6 +2,6 @@
 from __future__ import (division, absolute_import, print_function, unicode_literals)
 
 __author__ = 'XaviTorello'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .FACe import FACe_signer
