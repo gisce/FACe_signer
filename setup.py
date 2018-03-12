d55117e4491073b0f13b3a766e74d5c8ee9261b6
Xavi Torelló <info@xaviertorello.cat>
2018-03-12 10:35:17 +0100

    Avoid install dev requirements + fix version import

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import FACe_signer
+from FACe_signer import __version__
 
 with open('requirements.txt', 'r') as f:
     INSTALL_REQUIRES = f.readlines()
@@ -8,13 +8,12 @@ with open('requirements.txt', 'r') as f:
 setup(
     name='FACe signer',
     description='Zeep plugin that signs FACe SOAP requests',
-    version=FACe_signer.__version__,
+    version=__version__,
     url='http://www.gisce.net',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
-    tests_require=TESTS_REQUIRES,
     license='GNU GPLv3',
     provides=['FACe_signer'],
     classifiers = [
